d35d77cb1278e698a3ea36482c93bc6a359de7d3
latrondav <89303631+latrondav@users.noreply.github.com>
2023-02-19 01:39:12 +0300

    progress

--- regfunction.php
+++ regfunction.php
@@ -1,16 +1,14 @@
 <?php
     // Import the dbconnection.php file
     require_once('dbconnection.php');
+    include 'remainingseats.php';
 
-    // Retrieve the form data sent using AJAX
-    $data = json_decode(file_get_contents('php://input'), true);
-
-    // Extract the form data
-    $firstName = $data['firstName'];
-    $lastName = $data['lastName'];
-    $studentID = $data['studentID'];
-    $email = $data['email'];
-    $timeSlot = $data['timeSlot'];
+    // Retrieve the form data sent using POST
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $studentID = $_POST['studentID'];
+    $email = $_POST['email'];
+    $timeSlot = $_POST['timeSlot'];
 
     // Create a database connection
     $conn = createConnection();
@@ -20,13 +18,28 @@
     $stmt->bind_param("sssss", $firstName, $lastName, $studentID, $email, $timeSlot);
 
     // Execute the SQL statement
-    $stmt->execute();
+    $success = $stmt->execute();
 
     // Close the database connection
     $stmt->close();
     $conn->close();
 
-    // Send a success response back to the client
-    echo "success";
+    // Send a response back to the client
+    // Set the appropriate message and CSS class based on the result of the registration
+    if ($success) {
+        // Update remaining seats
+        $remainingSeats[$_POST['timeSlot']] -= 1;
+        $configString = "<?php\n\n\$remainingSeats = " . var_export($remainingSeats, true) . ";\n\n?>";
+        file_put_contents('remainingseats.php', $configString);
+
+        $message = "Registration successful";
+        $cssClass = "success";
+    } else {
+        $message = "Registration failed";
+        $cssClass = "error";
+    }
+    // Redirect back to regpage.php with the message and CSS class as URL parameters
+    header("Location: regpage.php?message=$message&class=$cssClass");
+    exit;
 
 ?>
--- regpage.php
+++ regpage.php
@@ -1,3 +1,7 @@
+<?php
+include 'remainingseats.php';
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -8,7 +12,14 @@
 
 <body>
     <h1>Registration Form</h1>
-    <form id="registration-form" action="regfunction.php" method="post">
+
+    <?php if (isset($_GET['message']) && isset($_GET['class'])) : ?>
+        <div id="message" class="<?php echo $_GET['class']; ?>">
+            <?php echo $_GET['message']; ?>
+        </div>
+    <?php endif; ?>
+
+    <form action="regfunction.php" method="POST">
         <label for="firstName">First Name:</label>
         <input type="text" id="firstName" name="firstName" required>
 
@@ -24,23 +35,46 @@
         <label for="timeSlot">Time Slot:</label>
         <select id="timeSlot" name="timeSlot" required>
             <option value="">Please select a time slot</option>
-            <option value="1" data-remaining-seats="32">10:00am - 12:00pm (32 seats remaining)</option>
-            <option value="2" data-remaining-seats="32">1:00pm - 3:00pm (32 seats remaining)</option>
-            <option value="3" data-remaining-seats="32">4:00pm - 6:00pm (32 seats remaining)</option>
-            <option value="4" data-remaining-seats="32">7:00pm - 9:00pm (32 seats remaining)</option>
+            <option value="1" data-remaining-seats="<?php echo $remainingSeats['1']; ?>">10:00am - 12:00pm (<?php echo $remainingSeats['1']; ?> seats remaining)</option>
+            <option value="2" data-remaining-seats="<?php echo $remainingSeats['2']; ?>">1:00pm - 3:00pm (<?php echo $remainingSeats['2']; ?> seats remaining)</option>
+            <option value="3" data-remaining-seats="<?php echo $remainingSeats['3']; ?>">4:00pm - 6:00pm (<?php echo $remainingSeats['3']; ?> seats remaining)</option>
+            <option value="4" data-remaining-seats="<?php echo $remainingSeats['4']; ?>">7:00pm - 9:00pm (<?php echo $remainingSeats['4']; ?> seats remaining)</option>
         </select>
+
 
         <input type="submit" value="Submit">
     </form>
 
-    <?php if(isset($_GET['message'])): ?>
-        <div id="message">
-            <?php echo $_GET['message']; ?>
-        </div>
-    <?php endif; ?>
+
+
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="regpage.js"></script>
+    <script>
+        // Get the message element
+        const message = document.getElementById('message');
+
+        // Display the message
+        message.style.display = 'block';
+
+        // Set a timeout to make the message disappear after 5 seconds
+        setTimeout(() => {
+            message.style.display = 'none';
+        }, 5000);
+    </script>
+
+    <script>
+        // Get the selected option element
+        const selectedOption = document.querySelector('#timeSlot option:checked');
+
+        // Decrement the remaining seats
+        const remainingSeats = selectedOption.dataset.remainingSeats - 1;
+        selectedOption.dataset.remainingSeats = remainingSeats;
+
+        // Update the text of the selected option to reflect the new remaining seats
+        selectedOption.text = selectedOption.text.replace(/\d+ seats remaining/, remainingSeats + ' seats remaining');
+    </script>
+
+    <!-- <script src="regpage.js"></script> -->
 </body>
 
 </html>
