016c7629ec13ea66e645160da951cb0f79bafab6
latrondav <89303631+latrondav@users.noreply.github.com>
2023-02-22 10:00:10 +0300

    progress

--- regpage.js
+++
@@ -1,4 +0,0 @@
-// Fade out the message box after 3 seconds
-setTimeout(function() {
-    $('#message-box').fadeOut('slow');
-}, 3000);
--- regtable.sql
+++ regtable.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 19, 2023 at 02:30 PM
+-- Generation Time: Feb 22, 2023 at 06:57 AM
 -- Server version: 8.0.27
 -- PHP Version: 7.4.26
 
@@ -37,17 +37,6 @@ CREATE TABLE IF NOT EXISTS `regtable` (
   `registrationTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`studentID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `regtable`
---
-
-INSERT INTO `regtable` (`studentID`, `firstName`, `lastName`, `email`, `timeSlot`, `registrationTime`) VALUES
-('1900702573', 'LATIM', 'RONNIE DAVID', 'latrondav@gmail.com', '10:00 - 12:00', '2023-02-19 14:04:10'),
-('1900702576', 'LATIM', 'DAVID', 'latrondav20201@outlook.com', '19:00 - 21:00', '2023-02-19 14:04:48'),
-('1900702572', 'LATIM', 'RONNIE DAVID', 'latrondav@gmail.com', '13:00 - 15:00', '2023-02-19 14:26:47'),
-('19007025712', 'LATIM', 'DAVID', 'latrondav20201@outlook.com', '16:00 - 18:00', '2023-02-19 14:27:20'),
-('1900702574', 'LATIM', 'RONNIE DAVID', 'latrondav@gmail.com', '19:00 - 21:00', '2023-02-19 14:28:27');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
